347a46722b86cc12e35eadb5185bee2e5b34e417
2gaga <yunjk0515@gmail.com>
2026-01-29 16:19:49 +0900

    fix: real final case sensitivity fix

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -1,7 +1,6 @@
 import Footer from '../../components/common/Footer/Footer';
 import Header from '../../components/common/Header/Header';
 import MyMenu from '../../components/mypage/MyMenu';
-import './style.css';
 
 const Mypage = () => {
     return (
